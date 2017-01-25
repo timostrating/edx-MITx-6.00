5bd16870f916ad07465dda92942b476c1de3c3d0
Timo Strating <timostrating@home.nl>
2017-01-26 00:56:53 +0100

    tmp

---
+++ tmp.py
@@ -0,0 +1,11 @@
+varA = 5
+varB = 5
+if type(varA) is str  or  type(varA) is str:
+    print("string involved")
+else:
+    if varA > varB:
+        print("bigger")
+    elif varA == varB:
+        print("equal")
+    elif varA < varB:
+        print("smaller")
